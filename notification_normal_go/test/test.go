1e5b1d2d30cfcffb1c9a4ffae9f21653b201ade7
zhengyuhang.0926 <zhengyuhang.0926@bytedance.com>
2019-03-07 22:22:35 +0800

    feat: begin mining after create account
    Change-Id: Idaa8d06ddfdaf06c588f4c174da88f0c981c79d3

--- notification_normal_go/test/test.go
+++ notification_normal_go/test/test.go
@@ -1,36 +1,42 @@
+
 //package notification_normal_go
 
 package main
 
 import (
-	"../utils"
-	"context"
 	"fmt"
+	"context"
 	"github.com/ethereum/go-ethereum/common"
 	"github.com/ethereum/go-ethereum/rpc"
+	"../utils"
+	"time"
 )
 
-func sendMultiTransaction(client *rpc.Client, from string, to common.Address, number int) {
 
-	for i := 0; i < number; i++ {
+func sendMultiTransaction(client *rpc.Client,from string,to common.Address,number int){
 
-		data := utils.EncryptTransactionInput("cadd:" + "hash" + string(i) + ":" + "contractHash")
+	for i:=0;i<number;i++{
+
+		data:=utils.EncryptTransactionInput("cadd:"+"hash"+string(i)+":"+"contractHash")
 
 		fmt.Println(data)
 
-		message := utils.NewMessage(common.HexToAddress(from), &to, "0x10", "0x"+data, "0x295f05", "0x77359400")
+		message:=utils.NewMessage(common.HexToAddress(from),&to,"0x10","0x"+data,"0x295f05","0x77359400")
 
-		txHash, err := utils.SendTransaction(client, &message, "abc", context.TODO())
+		txHash,err:=utils.SendTransaction(client,&message,"abc",context.TODO())
 		//txHash,err:=sendRawTransaction(client,"")
-		if err != nil {
-			fmt.Println("send new transaction fail: ", err)
+		if err!=nil{
+			fmt.Println("send new transaction fail: ",err)
 			break
 		}
 		fmt.Println(txHash)
 
 	}
 
+
+
 }
+
 
 func main() {
 
@@ -44,18 +50,25 @@ func main() {
 
 	// create account
 	myaccount := utils.CreateAccount(client, "abc")
-	if myaccount == "" {
+	if myaccount == ""{
 		fmt.Println("account create failed")
+		return
 	}
+	fmt.Println("New account", myaccount)
 
 	to := common.HexToAddress(myaccount)
 	fmt.Println("to: ", to)
 
-	err := utils.UnlockAccount(client, myaccount, "abc")
+	// set ether base
+	fmt.Println("set Etherbase: ", utils.SetEtherBase(client, myaccount))
+	// unlock
+	fmt.Println( "Account unlock", utils.UnlockAccount(client, myaccount, "abc"))
+	// begin mine
+	fmt.Println("start miner: ", utils.StartMiner(client, 1))
+	time.Sleep(60000000000)	// miner for 1 minute
 
-	if err != nil {
-		fmt.Println("unlockAccount fail: ", err)
-	}
+	// print balance
+	fmt.Println(utils.GetBalance(client, myaccount))
+
 	sendMultiTransaction(client, myaccount, to, 2)
-
 }
